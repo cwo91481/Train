7535e636691ba54c250466203ef874690f9e0ae8
Chuck <33357279+cwo91481@users.noreply.github.com>
2018-03-03 00:21:48 -0600

    mark1

---
+++ assets/train.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+	console.log("ready()");
+});
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAilD54eJKjznAibYNjW3pE-TvU64nRWkI",
+    authDomain: "train-project-ed9e3.firebaseapp.com",
+    databaseURL: "https://train-project-ed9e3.firebaseio.com",
+    projectId: "train-project-ed9e3",
+    storageBucket: "train-project-ed9e3.appspot.com",
+    messagingSenderId: "379502101864"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+    
+var trainName = "";
+var destination = "";
+var firstTime = "";
+var frequency = "";
+$("#formsubmit").on("click", function() {
+	trainName = $("#train-name").val().trim();
+	destination = $("#destination").val().trim();
+	firstTime = $("#first-time").val().trim();
+	frequency = $("#frequency").val().trim();
+
+	if (
+		trainName === "" 
+		|| destination === "" 
+		|| firstTime === "" 
+		|| frequency === ""
+	) {
+		console.log("empty form, return");
+		return false;//prevent page refresh
+	}
+
+	console.log(trainName);
+	console.log(destination);
+	console.log(firstTime);
+	console.log(frequency);
+
+	database.ref().push({
+		trainName: trainName,
+		destination: destination,
+		firstTime: firstTime,
+		frequency: frequency,
+		dateAdded: firebase.database.ServerValue.TIMESTAMP
+	});
+
+	return false;//prevent page refresh
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+
+	if(childSnapshot.val() == null) {
+		return;
+	}
+
+	console.log(childSnapshot.val());
+	
+	var trainEntry = $("<div>").addClass("col-md-12");
+
+	var covertedFirstTime = moment(childSnapshot.val().firstTime, "hh:mm").subtract(1, "years");
+
+	var diffTime = moment().diff(covertedFirstTime, "minutes");
+
+	var tRemainder = diffTime % childSnapshot.val().frequency;
+
+	// 16 - 00 = 16
+	// 16 % 7 = 2 (Modulus is the remainder)
+	// 7 - 2 = 5 minutes away
+	// 5 + 3:16 = 3:21
+	var tMinutesTillTrain = childSnapshot.val().frequency - tRemainder;
+	var nextTrain = moment().add(tMinutesTillTrain, "minutes")
+
+	trainEntry.append($("<div>").addClass("col-md-5ths").text(childSnapshot.val().trainName));
+	trainEntry.append($("<div>").addClass("col-md-5ths").text(childSnapshot.val().destination));
+	trainEntry.append($("<div>").addClass("col-md-5ths").text(childSnapshot.val().frequency));
+	trainEntry.append($("<div>").addClass("col-md-5ths").text(nextTrain.format("hh:mm a")));
+	trainEntry.append($("<div>").addClass("col-md-5ths").text(tMinutesTillTrain));
+
+	$("#trains").append($("<div class='row trainrow'>").append(trainEntry));
+}, function(errorObject) {
+	console.log("Errors handled: " + errorObject.code);
+})
